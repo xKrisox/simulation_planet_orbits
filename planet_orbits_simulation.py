07ff68bc5bf44a8e6045fff1c3f7b4231b5e02f3
Krzysztof Strzelecki <s185511@student.pg.edu.pl>
2025-07-17 12:34:27 +0200

    pygame session init

--- planet_orbits_simulation.py
+++ planet_orbits_simulation.py
@@ -1,1 +1,11 @@
 import pygame
+import math
+
+pygame.init()
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Planet Simulation')
+clock = pygame.time.Clock()
+
+G = 6.67430e-11
